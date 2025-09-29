477029a10f119f6ea7d7f7ba309789268ada9aa7
Konrad <konrvd.kvzx@gmail.com>
2025-09-29 21:45:25 +0200

    adjust settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/wsparcie-rozwoju-dziecka/",
   plugins: [react()],
+  base: "/wspieranie-rozwoju-dziecka",
 })

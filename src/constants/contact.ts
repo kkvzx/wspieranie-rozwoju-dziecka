288861a9b37f8e23cd90b132b161b26022ab547e
Konrad <konrvd.kvzx@gmail.com>
2025-10-19 13:17:37 +0200

    Change phone and email address

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,3 +1,2 @@
-export const ENCODED_PHONE_NUMBER = 'KzQ4IDEyMyA0NTYgNzg5'
-// export const ENCODED_EMAIL_ADDRESS = 'd3NwaWVyYW5pZS1yb3p3b2p1QGdtYWlsLmNvbQ==';
-export const ENCODED_EMAIL_ADDRESS = "eHl6QGdtYWlsLmNvbQ=="
+export const ENCODED_PHONE_NUMBER = 'KzQ4IDczNCA0NjEgMzU4'
+export const ENCODED_EMAIL_ADDRESS = "c3lsd2lhLmthenVzZWtAZ21haWwuY29t"
